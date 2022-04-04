9e2694e6d9999209bcb4262819267326cf5e3a78
vfor <trandangvu2708@gmail.com>
2022-04-04 22:52:02 +0700

    using lambda to change instance variable's value

--- src/pakageA/Lambda/LambdaDemo.java
+++ src/pakageA/Lambda/LambdaDemo.java
@@ -1,11 +1,28 @@
 package pakageA.Lambda;
 
 public class LambdaDemo {
+
+    Integer i;
+
+    public double lambdaModifyInstanceVariable(){
+        MyNum mNum = () ->{
+          i = 5;
+          return i;
+        };
+        return mNum.getVal();
+    }
+
     public static void main(String[] args) {
         MyNum myNum;
+        LambdaDemo lambdaDemo = new LambdaDemo();
+        Integer a = 5;
+        myNum = () -> {
+            int v = a + 5;
 
-        myNum = () -> 123.456;
+            return v;
+        };
 
         System.out.println(myNum.getVal());
+        System.out.println(lambdaDemo.lambdaModifyInstanceVariable());
     }
 }
